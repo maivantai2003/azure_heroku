d29fab5343fcca15214b735e029249a98d4f6db1
Van Tai <vantai08122003@gmail.com>
2024-12-05 10:07:33 +0700

    update

--- target/generated-sources/annotations/com/nhom27/nhatkykhambenh/mapper/LichHenKhamMapperImpl.java
+++ target/generated-sources/annotations/com/nhom27/nhatkykhambenh/mapper/LichHenKhamMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-12-05T06:16:52+0700",
+    date = "2024-12-05T10:06:32+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.4 (Oracle Corporation)"
 )
 @Component
